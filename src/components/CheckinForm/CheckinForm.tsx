eb9a02ba21cb2a2348de4ba29a6a19b14dc352e3
Kevinn Vannucci <kevinnvannucci0@gmail.com>
2025-07-11 12:52:50 -0300

    a

--- src/components/CheckinForm/CheckinForm.tsx
+++ src/components/CheckinForm/CheckinForm.tsx
@@ -2,7 +2,7 @@ import { useForm, type SubmitHandler } from 'react-hook-form';
 import { CheckinButton } from '../CheckinButton/CheckinButton';
 import HandleSubmit from '../../services/handleForm';
 
-<<<<<<< HEAD
+
 // Style 
 import {
   Container,
@@ -13,77 +13,6 @@ import {
   Label,
   Input,
 } from '../../styles/CheckinForm.styles';
-=======
-const Container = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: ${({ theme }) => theme.spacing.lg};
-
-  @media (max-width: 768px) {
-    padding: ${({ theme }) => theme.spacing.md};
-  }
-
-  @media (max-width: 480px) {
-    padding: ${({ theme }) => theme.spacing.sm};
-  }
-`;
-
-
-const Title = styled.h2`
-  font-size: 1.25rem;
-  color: ${({ theme }) => theme.colors.blueDark};
-  margin-bottom: ${({ theme }) => theme.spacing.md};
-`;
-
-const FormBox = styled.form`
-  background-color: ${({ theme }) => theme.colors.white};
-  padding: ${({ theme }) => theme.spacing.lg};
-  width: 100%;
-  max-width: 1100px;
-  display: flex;
-  flex-direction: column;
-  gap: ${({ theme }) => theme.spacing.lg};
-`;
-
-const FormGrid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: ${({ theme }) => theme.spacing.md} ${({ theme }) => theme.spacing.xl};
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
-    grid-template-columns: 1fr;
-  }
-`;
-
-
-const InputGroup = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const Label = styled.label`
-  font-size: 0.95rem;
-  font-weight: 500;
-  color: #333;
-  margin-bottom: 4px;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 12px;
-  border: none;
-  box-shadow: 0 6px 12px -2px ${({ theme }) => theme.colors.pinkShadow}80;
-  background-color: ${({ theme }) => theme.colors.greyLight};
-  border-radius: 100px;
-  font-size: 1rem;
-  outline-color: #aaa;
-
-  &::placeholder {
-    color: #aaa;
-  }
-`;
->>>>>>> 5af6a04e224fc7c9b10e80f4c772867718b2c351
 
 type Inputs = {
   name: string;
