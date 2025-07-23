aea668a010342700a0b0c8b663b4c239173d31d4
Kevinn Vannucci <kevinnvannucci0@gmail.com>
2025-07-23 00:38:10 -0300

    style: alterado a cor no header e alguns ajustes de tamanhos e responsividade

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import logo from '../../assets/whiteLogo.png';
-
-import { HeaderContainer, Logo, Title, RightSpace } from '../../styles/Header.style';
+import { HeaderContainer, Logo, Title } from '../../styles/Header.style';
 
 const Header: React.FC = () => (
-  <HeaderContainer role="banner" aria-label="Cabeçalho do site">
-    <Logo src={logo} alt="Healz Logo" />
+  <HeaderContainer role='banner' aria-label='Cabeçalho do site'>
+    <Logo src={logo} alt='Healz Logo' />
     <Title>Bem-vindo(a) ao Check-in</Title>
-    <RightSpace aria-hidden="true" />
   </HeaderContainer>
 );
 
--- src/styles/Header.style.ts
+++ src/styles/Header.style.ts
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
-  position: relative;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: ${({ theme }) => theme.spacing.md} ${({ theme }) => theme.spacing.lg};
-  background-color: ${({ theme }) => theme.colors.blueDark};
+  background: linear-gradient(100deg, #111111 50%, #7e0741 140%);
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
     padding: ${({ theme }) => theme.spacing.sm} ${({ theme }) => theme.spacing.md};
@@ -21,35 +20,20 @@ export const HeaderContainer = styled.header`
 `;
 
 export const Logo = styled.img`
-  height: 40px;
-  width: auto;
+  max-height: 40px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-    height: 30px;
+    max-height: 30px;
+    margin-bottom: 10px;
   }
 `;
 
 export const Title = styled.h1`
-  font-size: 3rem;
+  font-size: clamp(1.5rem, 5vw, 3rem);
   font-weight: 500;
   color: ${({ theme }) => theme.colors.white};
   font-family: ${({ theme }) => theme.fonts.main};
   line-height: 1.5;
   text-align: center;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
-    font-size: 2rem;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-    font-size: 1.5rem;
-  }
-`;
-
-export const RightSpace = styled.div`
-  width: 2.5rem;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-    display: none;
-  }
+  margin: auto;
 `;
