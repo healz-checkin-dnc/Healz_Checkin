4fe34a0d6e44f18e87feb79ce5dbb77667e15444
Kevinn Vannucci <kevinnvannucci0@gmail.com>
2025-07-24 10:02:45 -0300

    fix: corrigindo

--- src/services/handleForm.ts
+++ src/services/handleForm.ts
@@ -1,4 +1,4 @@
-import type { CheckinSchemaType } from '../schemas/CheckinSchema';
+import type { CheckinSchemaType } from '../schemas/CheckinSchemaZod';
 
 export default class HandleSubmit {
   private static API_URL =
